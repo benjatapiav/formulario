fa3e1f7999db8d98f051d082712bc8ee4fb21641
benjatapiav <benja.tapiav@duocuc.cl>
2024-04-23 14:26:18 -0400

    agregar

---
+++ formulario_funcion.js
@@ -0,0 +1,18 @@
+function mostrar(){
+    var btn=document.querySelector("#boton");
+    
+    var nombre = document.querySelector("#nombre").value;
+    var appaterno = document.querySelector("#appaterno").value;
+    var apmaterno = document.querySelector("#apmaterno").value;
+    var rut = document.querySelector("#rut").value;
+    var edad = document.querySelector("#edad").value;
+    var celular = document.querySelector("#celular").value;
+
+    document.querySelector("#nom").innerHTML = "Nombre: "+nombre;
+    document.querySelector("#app").innerHTML = "Apellido Paterno: "+appaterno;
+    document.querySelector("#apm").innerHTML = "Apellido Materno: "+apmaterno;
+    document.querySelector("#run").innerHTML = "Rut: "+rut;
+    document.querySelector("#cel").innerHTML = "Celular: "+celular;
+    document.querySelector("#año").innerHTML = "Edad: "+edad;
+
+}
